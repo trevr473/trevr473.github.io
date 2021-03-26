65a5a5d9973a6efebc3aadbf124fd430d9204950
trevr473 <trevr473@newschool.edu>
2021-03-26 14:46:40 +0100

    lol

---
+++ tour 2/script.js
@@ -0,0 +1,12 @@
+console.log("my js is working!!!"); 
+
+// get all elements that have class name of main container 2 
+var MainContainers = document.getElementsByClassName("main-container") 
+
+console.log("main-container");
+
+// add a click, mouseenter, mouseover handler to each main container 2 
+
+// when the user click on a main container 2, show elements in container-3 
+
+
