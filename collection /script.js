8ceee7c2a1979a442132106e05d34e8e2a59b8e3
trevr473 <trevr473@newschool.edu>
2021-04-14 21:52:15 +0200

    scroll

--- collection /script.js
+++ collection /script.js
@@ -1,25 +1,27 @@
 function ScrollDiv(){
-	let textElementLeft = document.getElementById('left');
-	let scrollTopLeft = textElement.scrollTop;
+	console.log("working")
+	let textElementLeft = document.getElementById("left");
+	let scrollTopLeft = textElementLeft.scrollTop;
 	if (scrollTopLeft < (textElementLeft.scrollHeight - textElementLeft.offsetHeight)) {
 		scrollTopLeft = scrollTopLeft + 1 
-}
-
-else {
-	scrollTopLeft = 0;
-}
-
-
-	let textElementRight = document.getElementById('right');
-	let scrollTopRight = textElement.scrollTop;
-	if (scrollTopRight < (textElementRight.scrollHeight - textElementRight.offsetHeight)) {
-		scrollTopRight = scrollTopRight - 1 
-}
+	}	
+	else {
+		scrollTopLeft = 0;
+	}
 
 
-else {
-	scrollTopRight = 0;
-}
+		//let textElementRight = document.getElementById("right");
+		//let scrollTopRight = textElementRight.scrollTop;
+		//if (scrollTopRight < (textElementRight.scrollHeight - textElementRight.offsetHeight)) {
+			//scrollTopRight = scrollTopRight - 1 
+	//}
 
+
+	//else {
+	//	scrollTopRight = 0;
+	//}
 
 }
+
+setInterval(ScrollDiv,60)
+
