86e709a56fb503300bbf621637e5b275c92e56ef
trevr473 <trevr473@newschool.edu>
2021-04-15 19:45:03 +0200

    push up

--- elastic-3/script.js
+++ elastic-3/script.js
@@ -1,27 +1,39 @@
-function ScrollDiv(){
-	console.log("working")
+function scrollLeftColumn(){
 	let textElementLeft = document.getElementById("left");
 	let scrollTopLeft = textElementLeft.scrollTop;
 	if (scrollTopLeft < (textElementLeft.scrollHeight - textElementLeft.offsetHeight)) {
-		scrollTopLeft = scrollTopLeft + 1 
+    setTimeout(function(){ 
+      console.log('scroll left div up');
+      scrollTopLeft = scrollTopLeft + 100;
+      console.log(scrollTopLeft);
+      textElementLeft.scrollTop = scrollTopLeft;
+      scrollLeftColumn();
+    }, 1000);
 	}	
 	else {
 		scrollTopLeft = 0;
 	}
-
-
-		//let textElementRight = document.getElementById("right");
-		//let scrollTopRight = textElementRight.scrollTop;
-		//if (scrollTopRight < (textElementRight.scrollHeight - textElementRight.offsetHeight)) {
-			//scrollTopRight = scrollTopRight - 1 
-	//}
-
-
-	//else {
-	//	scrollTopRight = 0;
-	//}
-
 }
+function scrollRightColumn(){
+	let textElementRight = document.getElementById("right");
+	let scrollTopRight = textElementRight.scrollTop;
+	if (scrollTopRight < (textElementRight.scrollHeight - textElementRight.offsetHeight)) {
+    setTimeout(function(){ 
+      console.log('scroll right div up');
+      scrollTopRight = scrollTopRight + 100;
+      console.log(scrollTopRight);
+      textElementRight.scrollTop = scrollTopRight;
+      scrollRightColumn();
+    }, 1000);
+	}	
+	else {
+		scrollTopRight = 0;
+	}
+}
+scrollLeftColumn();
+scrollRightColumn();
 
-setInterval(ScrollDiv,60)
-
+function togglePopup(){
+  document.getElementById("popup-1").classList.toggle("active");
+}
+	
