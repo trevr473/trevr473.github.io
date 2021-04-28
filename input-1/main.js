db1c94c11d1a14d49d8e8fd04517e3cbc9b7abc8
trevr473 <trevr473@newschool.edu>
2021-04-28 18:51:15 +0200

    ok

--- input-1/main.js
+++ input-1/main.js
@@ -1,18 +1,18 @@
 window.onload = function () {
 
 	// select the thing we wanna drag 
-	var collection1 = document.getElementById('image');
+	var object = document.getElementById('image');
 
 	//listen to the touchmove event, every time it fires, grab the location of the touch 
 	//then assign it to collection 1 
 
-	collection1.addEventListener('touchmove', function(ev){
+	object.addEventListener('touchmove', function(ev){
 
 		//grab the location of the touch 
 		var touchLocation = ev.targetTouches[0];
 
 	    //assign collection1 new coordinates based on the touch 
-	    collection1.style.left = touchLocation.pageX + 'px'; 
-	    collection1.style.top = touchLocation.pageY + 'px';
+	    object.style.left = touchLocation.pageX + 'px'; 
+	    object.style.top = touchLocation.pageY + 'px';
         })
 }
